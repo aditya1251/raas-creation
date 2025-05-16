2ee1904e33f62bd078c41486e76e9c7deeec355e
Aditya Bansal <adityabansalsafe@gmail.com>
2025-05-16 12:14:55 +0530

    Fix contact, product perfomance

--- components/admin/Topperfomer.tsx
+++ components/admin/Topperfomer.tsx
@@ -1,26 +1,27 @@
 "use client";
 import { ProductPerformanceApi } from "@/lib/api/productperformance";
 import { useQuery } from "@tanstack/react-query";
-import {
-  PieChart,
-  Pie,
-  Cell,
-  ResponsiveContainer,
-  Tooltip,
-} from "recharts";
+import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip } from "recharts";
 
-const COLORS = ["#6366F1", "#10B981", "#F59E0B", "#EF4444", "#3B82F6", "#8B5CF6"];
+const COLORS = [
+  "#6366F1",
+  "#10B981",
+  "#F59E0B",
+  "#EF4444",
+  "#3B82F6",
+  "#8B5CF6",
+];
 
-export function ProductCategoryChart() {
+export function Topperfomer() {
   const {
     data: products,
     isLoading,
     isError,
   } = useQuery({
-    queryKey: ["productPerformance"],
+    queryKey: ["productTopPerformance"],
     queryFn: async () => {
       try {
-        return await ProductPerformanceApi.getAll();
+        return await ProductPerformanceApi.getTopPerfomer();
       } catch (error) {
         return { data: {} };
       }
@@ -29,7 +30,7 @@ export function ProductCategoryChart() {
 
   const categorySales = products?.data
     ? Object.values(products.data).map((item) => ({
-        name: item.categoryName,
+        name: item.productName,
         value: item.totalRevenue,
       }))
     : [];
@@ -37,7 +38,7 @@ export function ProductCategoryChart() {
   return (
     <div className="bg-white rounded-2xl shadow-md p-6 w-full">
       <h2 className="text-2xl font-semibold text-gray-800 mb-6">
-        Sales by Category
+        Top Performing Products
       </h2>
 
       <div className="flex flex-col md:flex-row items-center justify-between gap-6">
@@ -45,7 +46,7 @@ export function ProductCategoryChart() {
           {isLoading ? (
             <p className="text-gray-500 text-center">Loading...</p>
           ) : isError || categorySales.length === 0 ? (
-            <p className="text-red-500 text-center">No Product Category Found</p>
+            <p className="text-red-500 text-center">No Product Found</p>
           ) : (
             <ResponsiveContainer width="100%" height="100%">
               <PieChart>
@@ -55,8 +56,7 @@ export function ProductCategoryChart() {
                   cy="50%"
                   outerRadius={100}
                   fill="#8884d8"
-                  dataKey="value"
-                >
+                  dataKey="value">
                   {categorySales.map((_, index) => (
                     <Cell
                       key={`cell-${index}`}
@@ -81,9 +81,13 @@ export function ProductCategoryChart() {
                   className="w-4 h-4 rounded-full"
                   style={{ backgroundColor: COLORS[index % COLORS.length] }}
                 />
-                <span className="text-gray-700 font-medium">
+                <span
+                  className="text-gray-700 font-medium truncate max-w-[200px]"
+                  title={item.name} // for tooltip on hover to show full name
+                >
                   {item.name}:
                 </span>
+
                 <span className="ml-auto text-gray-900 font-semibold">
                   ₹{item.value}
                 </span>
